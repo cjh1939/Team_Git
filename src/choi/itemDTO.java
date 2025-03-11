90b4b4ec012e9a64746e7f72b954457e8573cfd2
cjh1939 <chemddhkd@gmail.com>
2025-03-11 11:38:54 +0900

    아이템기능 10%완성

--- src/choi/itemDTO.java
+++ src/choi/itemDTO.java
@@ -2,5 +2,6 @@ package choi;
 
 //최팀원은 여기서 코드작성
 public class itemDTO {
-
+  private  int itemNo;
+  private  String itemname;
 }
--- src/park/cartDTO.java
+++ src/park/cartDTO.java
@@ -2,5 +2,6 @@ package park;
 
 //박팀원은 여기에 코드 작성
 public class cartDTO {
+  private  int cartNUm;
 
 }
